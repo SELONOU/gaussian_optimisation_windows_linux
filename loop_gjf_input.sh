3bd4196f4d4b489b21e4ff67439dff1065c87ce3
KANKİNOU SELONOU GAUTİER <kankinounoatedegautier@yahoo.fr>
2024-11-03 23:42:54 +0300

    Update loop_gjf_input.sh

--- loop_gjf_input.sh
+++ loop_gjf_input.sh
@@ -4,4 +4,5 @@ sed -i '1,3d' "$mol".gjf
 cat criteria_truba "$mol".gjf > "$mol".com
 sed -i '3s/.*/%chk='"$mol"'.chk/' "$mol".com # replace the entire 3rd line by %chk=
 sed -i 's/Title Card Required/'"$mol"'/g' "$mol".com
+sed -i 's/\r//g' "$mol".com  #removes carriage return characters 
 done < files
